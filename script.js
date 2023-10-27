f32e140c1a6f3b0618805c9fc96a5b7d22419c70
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-10-27 12:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+ const savedUsername = localStorage.getItem("username");
+        const savedPassword = localStorage.getItem("password");
+
+        if (savedUsername && savedPassword) {
+            document.getElementById("existing").style.display = "block";
+        }
+
+        // Function to handle form submission
+        document.getElementById("loginForm").addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            const rememberMe = document.getElementById("rememberMe").checked;
+
+            if (rememberMe) {
+                localStorage.setItem("username", username);
+                localStorage.setItem("password", password);
+            } else {
+                localStorage.removeItem("username");
+                localStorage.removeItem("password");
+            }
+
+            alert("Logged in as " + username);
+        });
+
+        // Function to handle existing user login button
+        document.getElementById("existing").addEventListener("click", function () {
+            alert("Logged in as " + savedUsername);
+        });
